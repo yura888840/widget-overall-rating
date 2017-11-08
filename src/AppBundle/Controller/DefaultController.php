99d5e4cb4e5e94ed8103e7d49cc59d7c633c27eb
Yuri <yura888840@gmail.com>
2017-11-08 13:29:26 +0200

    Repository with methid which calculates avarage rate + Controller response

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Hotel;
+use AppBundle\Repository\HotelRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,12 +14,25 @@ class DefaultController extends Controller
     /**
      * @Route("/widget/{UUID}.js", name="widget")
      */
-    public function indexAction(Request $request, $UUID)
+    public function indexAction(Request $request, $UUID, EntityManagerInterface $em)
     {
-        die('***');
-        // replace this example code with whatever you need
+        /** @var HotelRepository $hotelRepository */
+        $hotelRepository = $em->getRepository(Hotel::class);
+        $hotel = $hotelRepository
+            ->find($UUID);
+
+        if (!$hotel) {
+            throw new \RuntimeException('No hotel with UUID: ' . $UUID);
+        }
+
+        try {
+            $avgRating = $hotelRepository->getAverageRatingWithinLastYearForHotel($UUID);
+        } catch (\Exception $e) {
+            throw new \RuntimeException('Database error. (Need to handle it properly, but not implemented)');
+        }
+
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'avg_rate' => $avgRating
         ]);
     }
 }
--- src/AppBundle/Repository/HotelRepository.php
+++ src/AppBundle/Repository/HotelRepository.php
@@ -12,4 +12,21 @@ use Doctrine\ORM\EntityRepository;
 
 class HotelRepository extends EntityRepository
 {
+    /**
+     * @param string $UUID
+     * @return int
+     */
+    public function getAverageRatingWithinLastYearForHotel($UUID)
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+
+        $qb->select('avg(r.rating)')
+            ->from('AppBundle\Entity\Review', 'r')
+            ->where('r.hotel=:hotel and r.createdDate > :date')
+            ->setParameter('hotel', $UUID)
+            ->setParameter('date', new \DateTime('-1 year'));
+
+        return
+            (int) round($qb->getQuery()->getSingleScalarResult());
+    }
 }
